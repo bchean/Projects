28f841a3bd64b10d60c4422e275988a09d49f2af
Bo Chean <bchean@gmail.com>
2013-08-01 20:29:37 -0700

    Finished Numbers#Complex.

--- Numbers/complex.rb
+++ Numbers/complex.rb
@@ -1,16 +1,11 @@
 #!/usr/local/bin/ruby
 
 def usage_and_exit
-	puts 'Usage: ./complex.rb (add|mult) num1 num2'
+	puts 'Usage: ./complex.rb (add|sub|mul|div|neg|inv) num1 [num2]'
 	puts 'Nums must be specified as either a+bi or a,b.'
+	puts 'a and b must be integers.'
 	exit 1
 end
-
-if ARGV.length != 3
-	usage_and_exit
-end
-
-op, arg1, arg2 = ARGV[0..2]
 
 class ComplexNum
 	attr_reader :a, :b
@@ -28,11 +23,27 @@ class ComplexNum
 	end
 
 	def initialize(a, b=0)
-		@a, @b = a.to_i, b.to_i
+		@a, @b = a.to_f, b.to_f
+	end
+
+	def neg
+		ComplexNum.new(-@a, -@b)
+	end
+
+	def inv
+		ComplexNum.new(1, 0) / self
+	end
+
+	def conj
+		ComplexNum.new(@a, -@b)
 	end
 
 	def +(other)
 		ComplexNum.new(@a+other.a, @b+other.b)
+	end
+
+	def -(other)
+		self + other.neg
 	end
 
 	def *(other)
@@ -41,18 +52,41 @@ class ComplexNum
 		ComplexNum.new(real, imag)
 	end
 
+	def /(other)
+		temp = self * other.conj
+		denom = (other * other.conj).a
+		real = temp.a / denom
+		imag = temp.b / denom
+		ComplexNum.new(real, imag)
+	end
+
 	def to_s
 		"#{@a}#{'+' if @b>=0}#{@b}i"
 	end
 end
 
-num1, num2 = ComplexNum.parse(arg1), ComplexNum.parse(arg2)
+if ARGV.length != 2 and ARGV.length != 3
+	usage_and_exit
+end
+
+op, arg1, arg2 = ARGV[0..2]
+
+num1 = ComplexNum.parse(arg1)
+num2 = ComplexNum.parse(arg2) if arg2
 
 case op
 when 'add'
 	puts num1 + num2
-when 'mult'
+when 'sub'
+	puts num1 - num2
+when 'mul'
 	puts num1 * num2
+when 'div'
+	puts num1 / num2
+when 'neg'
+	puts num1.neg
+when 'inv'
+	puts num1.inv
 else
 	usage_and_exit
 end
